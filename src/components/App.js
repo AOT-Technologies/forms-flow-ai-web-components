71b427ccb1a8971a4585315d87a7004f0d66c1ea
AbijithS-aot <abijith.s@aot-technologies.com>
2022-10-20 16:11:42 +0530

    success message modifies

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import { publicApplicationCreate } from "../apiManager/services/appService";
 import { getFormUrl } from "../apiManager/services/formatterServices";
 
 const App =({src})=>{
+    const [isFormSubmitted,setIsFormsubmitted] = useState(false);
+    const message = document.querySelector('formsflow-wc').getAttribute('message');
+    console.log(message)
     const [url ,setUrl] =useState('');
     useEffect(()=>{
         console.log("new url")
@@ -13,6 +16,7 @@ const App =({src})=>{
         setUrl(newUrl);
     },[src])
    const handleSubmit = (data)=>{
+    setIsFormsubmitted(true)
     const formId = data.form;
     const submissionId = data._id;
     const formUrl = getFormUrl(formId,submissionId);
@@ -21,19 +25,23 @@ const App =({src})=>{
     }
     publicApplicationCreate(formData);
    } 
+   console.log("form submitted",isFormSubmitted)
     return(
         <div className="container">
-            <Form 
+            {!isFormSubmitted ? (<Form 
              src={url}
              onSubmit={(data)=>{
                 handleSubmit(data);
              }}
-             options = {{noAlerts : true,submitMessage:""}}
+             options = {{noAlerts : true}}
             
              onFormError={(error)=>{
                  console.log("error",error)
              }}
-            />
+            /> ): <div className="text-center pt-5">
+            <h1>{message}</h1>
+            <p>saved successfully</p>
+          </div>}
         </div>
     )
 }
