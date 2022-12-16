94253ef12f1709e8f4f7b65731bc1e23c6854053
AbijithS-aot <abijith.s@aot-technologies.com>
2022-12-16 22:07:11 +0530

    hybrid solution

--- src/apiManager/services/appService.js
+++ src/apiManager/services/appService.js
@@ -1,34 +1,48 @@
 import { httpGETRequest, httpPOSTRequest } from "../httpRequestHandler";
 
-export const publicApplicationCreate = (url,data)=>{
+export const applicationCreate = (url,data)=>{
     return  httpPOSTRequest(url,data,{headers: {"Authorization" : `Bearer ${localStorage.getItem('authToken')}`}});
 };
 
+export const publicApplicationCreate = (url,data)=>{
+    return  httpPOSTRequest(url,data);
+}
+
 export const fetchRoles = (url,callback)=>{
-    console.log("roles ur;",url)
      httpGETRequest(url, {headers: {"Authorization" : `Bearer ${localStorage.getItem('authToken')}`}}).then((res)=>{
             localStorage.setItem('formioToken',res.headers["x-jwt-token"])
-             callback(res)
+             callback(res.headers["x-jwt-token"])
         })
 }
 
 export const getForms = (url,token,callback)=>{
-    console.log("ivde hit aayi")
-        httpGETRequest(url,{headers: {"x-jwt-token" : localStorage.getItem('formioToken')}}).then((res)=>{
-            callback(res)
-        })
-    
+    if(token){
+        httpGETRequest(url,{headers: {"x-jwt-token" : token}}).then((res)=>{
+            callback(res);
+        });
+    }
 }
 
-export const formSubmission = (url,data,callback)=>{
-    httpPOSTRequest(url,data,{headers: {"x-jwt-token" : localStorage.getItem('formioToken')}}).then((res)=>{
-        callback(res);
-    });
+export const formSubmission = (url,data,anonymous,callback)=>{
+    if(anonymous){
+        console.log("anonymous aanu")
+        httpPOSTRequest(url,data).then((res)=>{
+            callback(res);
+        });
+    }
+    if(!anonymous){
+        console.log("anonymous alla")
+        httpPOSTRequest(url,data,{headers: {"x-jwt-token" : localStorage.getItem('formioToken')}}).then((res)=>{
+            callback(res);
+        });
+    }
+   
 }   
 
-export const verifyJWTtoken = (url,token)=>{
-    console.log("token",token)
+export const verifyJWTtoken = (url,token,callback)=>{
     httpPOSTRequest(url,{},{headers: {"Authorization" : `Bearer ${token}`}}).then((res)=>{
         localStorage.setItem('formioToken',res.data)
+        callback(res.data)
     });
 }
+
