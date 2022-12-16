675b8c9529bb0d916e9f5426971536794b2b16bc
AbijithS-aot <abijith.s@aot-technologies.com>
2022-12-16 22:07:11 +0530

    hybrid solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,46 @@
 import { useEffect, useState } from "react";
-import { fetchRoles, getForms, verifyJWTtoken } from "../apiManager/services/appService";
+import { fetchRoles,verifyJWTtoken } from "../apiManager/services/appService";
 import { initKeycloak } from "../services/UserServices";
 import RenderForms from "./RenderForms";
 
 const App = ({ src }) => {
   const [configFile,setConfigFile] = useState(null);
   const [jwt,setJwt] = useState(null);
+  const[formioToken,setFormioToken] = useState(null);
+  const [anonymous,setAnonymous] = useState(false);
 
   useEffect(()=>{
     setConfigFile(JSON.parse(document.querySelector("formsflow-wc").getAttribute("configFile")));
     setJwt(document.querySelector("formsflow-wc").getAttribute("token"));
   },[]);
   useEffect(()=>{
-   if(configFile && configFile.authenticationType === 'external'){
-    verifyJWTtoken("http://localhost:5000/application/external/verify",jwt)
-   }
-   if(configFile && configFile.authenticationType ==='internal' && !configFile.multitenancy){
-    
-   }
-  },[configFile]);
+    if(configFile && configFile.authenticationType === 'external'){
+      verifyJWTtoken("http://localhost:5000/application/external/verify",jwt,(data)=>{
+        setFormioToken(data);
+      });
+     };
+     if(configFile && configFile.authenticationType ==='internal' && !configFile.multitenancy){
+      let rolesUrl = `${configFile.webApiUrl}/api/formio/roles`
+      initKeycloak(configFile.keycloakUrl,configFile.realm,configFile.clientId,() => {
+        fetchRoles(rolesUrl,(data)=>{
+          setFormioToken(data);
+        });
+      });
+     };
+     if(configFile && configFile.authenticationType ==='internal' && configFile.multitenancy){
+      let rolesUrl = `${configFile.webApiUrl}/adminapi/api/v1/tenant`
+      initKeycloak(configFile.keycloakUrl,configFile.realm,configFile.clientId,() => {
+        fetchRoles(rolesUrl);
+      });
+     };
+     if(configFile && configFile.authenticationType ==='anonymous'){
+      setAnonymous(true);
+     };
+  },[configFile,jwt])
 
   return (
     <div className="container">
-     <RenderForms /> 
+     <RenderForms configFile={configFile} formioToken={formioToken} anonymous={anonymous}/> 
     </div>
   );
 };
