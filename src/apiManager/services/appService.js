d0fcca0b4990e841ee9c0cddbd9cc212b409f230
AbijithS-aot <abijith.s@aot-technologies.com>
2022-12-29 20:28:24 +0530

    hybrid solution

--- src/apiManager/services/appService.js
+++ src/apiManager/services/appService.js
@@ -1,50 +1,50 @@
 import { httpGETRequest, httpPOSTRequest } from "../httpRequestHandler";
 
-export const applicationCreate = (url,data)=>{
-    return  httpPOSTRequest(url,data,{headers: {"Authorization" : `Bearer ${localStorage.getItem('authToken')}`}});
+// API call for internal application create
+export const internalApplicationCreate = (url, data, callback) => {
+  return httpPOSTRequest(url, data, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+  }).then((res) => {
+    if (res) {
+      callback(res);
+    }
+  }).catch((err)=>{
+    console.error("error",err);
+  });
 };
 
-export const externalApplicationCreate = (url,authToken,data)=>{
-    return  httpPOSTRequest(url,data,{headers: {"Authorization" : `Bearer ${authToken}`}});
+//API call for external application create
+export const externalApplicationCreate = (url, authToken, data, callback) => {
+  return httpPOSTRequest(url, data, {
+    headers: { Authorization: `Bearer ${authToken}` },
+  }).then((res) => {
+    if (res) {
+      callback(res);
+    }
+  }).catch((err)=>{
+    console.error("error",err)
+  });
 };
 
-export const publicApplicationCreate = (url,data)=>{
-    return  httpPOSTRequest(url,data);
-}
+// API call for public application create
+export const publicApplicationCreate = (url, data) => {
+  return httpPOSTRequest(url, data).catch((err)=>{
+    console.error("error",err);
+  });
+};
 
-export const fetchRoles = (url,callback)=>{
-     httpGETRequest(url, {headers: {"Authorization" : `Bearer ${localStorage.getItem('authToken')}`}}).then((res)=>{
-            localStorage.setItem('formioToken',res.headers["x-jwt-token"])
-             callback(res.headers["x-jwt-token"])
-        })
-}
+// API call for getting external authentication form json
+export const getExternalAuthourizedForm = (url, token, callback) => {
+ return httpGETRequest(url, { headers: { Authorization: `Bearer ${token}` } })
+};
 
-export const getForms = (url,token,callback)=>{
-    if(token){
-        httpGETRequest(url,{headers: {"x-jwt-token" : token}}).then((res)=>{
-            callback(res);
-        });
+// API call for getting internal authentication form json
+export const getInternalAUthorizedForms = (url, token, callback) => {
+  httpGETRequest(url, { headers: { Authorization: `Bearer ${token}` } }).then(
+    (res) => {
+      callback(res.data);
     }
-}
-
-export const formSubmission = (url,data,anonymous,callback)=>{
-    if(anonymous){
-        httpPOSTRequest(url,data).then((res)=>{
-            callback(res);
-        });
-    }
-    if(!anonymous){
-        httpPOSTRequest(url,data,{headers: {"x-jwt-token" : localStorage.getItem('formioToken')}}).then((res)=>{
-            callback(res);
-        });
-    }
-   
-}   
-
-export const verifyJWTtoken = (url,token,callback)=>{
-    httpPOSTRequest(url,{},{headers: {"Authorization" : `Bearer ${token}`}}).then((res)=>{
-        localStorage.setItem('formioToken',res.data.access_token);
-        callback(res.data);
-    });
-}
-
+  ).catch((err)=>{
+    console.error("error",err)
+  });
+};
